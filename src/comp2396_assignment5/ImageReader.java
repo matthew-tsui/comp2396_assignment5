4c354710a1367b54586b159cd9c73112078f9993
Matthew <mhct123@gmail.com>
2019-04-24 01:40:11 +0800

    Initial Commit

---
+++ src/comp2396_assignment5/ImageReader.java
@@ -0,0 +1,62 @@
+package comp2396_assignment5;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+
+/**
+ * @author matthewtsui
+ * ImageReader class to read and resize image
+ */
+public class ImageReader {
+	/**
+	 * @return the resized image from file chooser
+	 */
+	public static BufferedImage load() {
+		JFileChooser fc = new JFileChooser();
+		fc.setDialogTitle("Choose an image");
+		int openReturnVal = fc.showOpenDialog(null);
+		
+		if (openReturnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			BufferedImage bimg = null;
+			try {
+				bimg = ImageIO.read(new File(file.getAbsolutePath()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if (bimg == null) {
+				//JavaPuzzle.showError();
+				return load();	
+			}
+			
+			BufferedImage resizedImage = resizeImage(bimg);
+			
+			return resizedImage;
+		} else {
+			//JavaPuzzle.showError();
+			return load();
+		}
+	}
+	
+	/**
+	 * @param image to be resize
+	 * @return resized image according to specification asss
+	 */
+	private static BufferedImage resizeImage(BufferedImage image) {
+		Image tmp = image.getScaledInstance(800, 800, Image.SCALE_DEFAULT);
+		BufferedImage resizedImage = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = resizedImage.createGraphics();
+	    g2d.drawImage(tmp, 0, 0, null);
+	    g2d.dispose();
+		return resizedImage;
+	}
+		
+	
+}
